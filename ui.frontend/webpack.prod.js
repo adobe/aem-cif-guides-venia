2659cdb43371d34ff443490e47e1c9ca11f9675f
Daniel Platon <platon@adobe.com>
2020-07-10 09:53:57 +0300

    CIF-1480 -Move the `react-components` project from the Core Components to the Venia Project
    Port the changes to the `main` branch

--- ui.frontend/webpack.prod.js
+++ ui.frontend/webpack.prod.js
@@ -1,7 +1,20 @@
-const merge                   = require('webpack-merge');
+/*******************************************************************************
+ *
+ *    Copyright 2020 Adobe. All rights reserved.
+ *    This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License. You may obtain a copy
+ *    of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *    OF ANY KIND, either express or implied. See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ ******************************************************************************/
+const merge = require('webpack-merge');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin            = require('terser-webpack-plugin');
-const common                  = require('./webpack.common.js');
+const TerserPlugin = require('terser-webpack-plugin');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'production',
@@ -12,22 +25,25 @@ module.exports = merge(common, {
             new OptimizeCSSAssetsPlugin({
                 cssProcessorPluginOptions: {
                     cssProcessor: require('cssnano'),
-                    preset: ['default', {
-                        calc: true,
-                        convertValues: true,
-                        discardComments: {
-                            removeAll: true
+                    preset: [
+                        'default',
+                        {
+                            calc: true,
+                            convertValues: true,
+                            discardComments: {
+                                removeAll: true,
+                            },
+                            discardDuplicates: true,
+                            discardEmpty: true,
+                            mergeRules: true,
+                            normalizeCharset: true,
+                            reduceInitial: true, // This is since IE11 does not support the value Initial
+                            svgo: true,
                         },
-                        discardDuplicates: true,
-                        discardEmpty: true,
-                        mergeRules: true,
-                        normalizeCharset: true,
-                        reduceInitial: true, // This is since IE11 does not support the value Initial
-                        svgo: true
-                    }],
+                    ],
                 },
-                canPrint: false
-            })
+                canPrint: false,
+            }),
         ],
         splitChunks: {
             cacheGroups: {
@@ -35,11 +51,11 @@ module.exports = merge(common, {
                     chunks: 'all',
                     name: 'site',
                     test: 'main',
-                    enforce: true
-                }
-            }
-        }
+                    enforce: true,
+                },
+            },
+        },
     },
     devtool: 'none',
-    performance: { hints: false }
+    performance: {hints: false},
 });
