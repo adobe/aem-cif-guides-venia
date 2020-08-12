1abacb1bcd3000d9e93083a2ddb8800b4640cf5e
Mark J. Becker <herzog31@users.noreply.github.com>
2020-08-12 16:02:25 +0200

    CIF-1546 - Refactor React portals and introduce ConfigContext

--- ui.frontend/src/main/components/App/App.js
+++ ui.frontend/src/main/components/App/App.js
@@ -15,24 +15,52 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { I18nextProvider } from 'react-i18next';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { CommerceApp, Cart, AuthBar, AccountContainer, AddressBook } from '@adobe/aem-core-cif-react-components';
+import {
+    CommerceApp,
+    Portal,
+    ConfigContextProvider,
+    Cart,
+    CartTrigger,
+    AuthBar,
+    AccountContainer,
+    AddressBook
+} from '@adobe/aem-core-cif-react-components';
 
 import i18n from './i18n';
-import config, { addressBookPath } from './config';
+import partialConfig from './config';
 
 import '../../site/main.scss';
 
 const App = () => {
     const { storeView, graphqlEndpoint } = document.querySelector('body').dataset;
+    const { mountingPoints, pagePaths } = partialConfig;
+    const config = {
+        ...partialConfig,
+        storeView,
+        graphqlEndpoint
+    };
 
     return (
         <I18nextProvider i18n={i18n} defaultNS="common">
-            <CommerceApp uri={graphqlEndpoint} storeView={storeView} config={config}>
-                <Cart />
-                <AuthBar />
-                <AccountContainer />
-                <Route path={addressBookPath} component={AddressBook} />
-            </CommerceApp>
+            <ConfigContextProvider config={config}>
+                <CommerceApp>
+                    <Portal selector={mountingPoints.cartTrigger}>
+                        <CartTrigger />
+                    </Portal>
+                    <Cart />
+                    <Portal selector={mountingPoints.authBarContainer}>
+                        <AuthBar />
+                    </Portal>
+                    <Portal selector={mountingPoints.accountContainer}>
+                        <AccountContainer />
+                    </Portal>
+                    <Route path={pagePaths.addressBook}>
+                        <Portal selector={mountingPoints.addressBookContainer}>
+                            <AddressBook />
+                        </Portal>
+                    </Route>
+                </CommerceApp>
+            </ConfigContextProvider>
         </I18nextProvider>
     );
 };
