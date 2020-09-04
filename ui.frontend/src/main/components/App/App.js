a7791d860f47dde879dae9b163b15b671b3562a8
Laurentiu Magureanu <magurean@adobe.com>
2020-09-04 16:51:46 +0300

    CIF-1473 - Support Bundle Products: option selection (#39)
    * Added ProductOptions component
    * Refactor React portals and introduce ConfigContext
    * Refactor options component
    * Add styles for bundle options
    * Add BundleProductOptions portal to App
    * add bundleProductOptionsContainer selector
    * update Bundle product options styling
    
    Co-authored-by: Mark J. Becker <herzog31@users.noreply.github.com>
    Co-authored-by: hbacila <53909563+hbacila@users.noreply.github.com>
    Co-authored-by: Daniel Platon <dplaton@pm.me>

--- ui.frontend/src/main/components/App/App.js
+++ ui.frontend/src/main/components/App/App.js
@@ -23,7 +23,8 @@ import {
     CartTrigger,
     AuthBar,
     AccountContainer,
-    AddressBook
+    AddressBook,
+    BundleProductOptions
 } from '@adobe/aem-core-cif-react-components';
 
 import i18n from './i18n';
@@ -61,6 +62,9 @@ const App = () => {
                             <AddressBook />
                         </Portal>
                     </Route>
+                    <Portal selector={mountingPoints.bundleProductOptionsContainer}>
+                        <BundleProductOptions />
+                    </Portal>
                 </CommerceApp>
             </ConfigContextProvider>
         </I18nextProvider>
