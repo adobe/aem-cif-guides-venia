8d7af7bc8bf73a1108cb095b48a8c82e8e131dcc
Laurentiu Magureanu <magurean@adobe.com>
2020-10-14 11:16:12 +0300

    CIF-1181: Add integration tests for datalayer

--- it.tests/src/main/java/com/venia/it/tests/CategoryPageIT.java
+++ it.tests/src/main/java/com/venia/it/tests/CategoryPageIT.java
@@ -14,6 +14,9 @@
 
 package com.venia.it.tests;
 
+import java.io.IOException;
+import java.util.stream.Collectors;
+
 import org.apache.sling.testing.clients.ClientException;
 import org.apache.sling.testing.clients.SlingHttpResponse;
 import org.jsoup.Jsoup;
@@ -22,13 +25,17 @@ import org.jsoup.select.Elements;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.venia.it.utils.Utils;
+
 public class CategoryPageIT extends CommerceTestBase {
 
     private static final String PRODUCTLIST_SELECTOR = ".productlist ";
     private static final String PRODUCTLIST_TITLE_SELECTOR = PRODUCTLIST_SELECTOR + ".category__categoryTitle";
+    private static final String PRODUCTLIST_GALLERY_SELECTOR = PRODUCTLIST_SELECTOR + ".gallery__root";
 
     @Test
-    public void testProductListPageWithSampleData() throws ClientException {
+    public void testProductListPageWithSampleData() throws ClientException, IOException {
         SlingHttpResponse response = adminAuthor.doGet(VENIA_CONTENT_US_EN_PRODUCTS_CATEGORY_PAGE + ".1.html", 200);
         Document doc = Jsoup.parse(response.getContent());
 
@@ -51,10 +58,24 @@ public class CategoryPageIT extends CommerceTestBase {
         // Check the number of root elements in the navigation menu
         elements = doc.select(NAVIGATION_ITEM_SELECTOR);
         Assert.assertEquals(7, elements.size());
+
+        // Verify category gallery datalayer
+        elements = doc.select(PRODUCTLIST_GALLERY_SELECTOR);
+        JsonNode result = Utils.OBJECT_MAPPER.readTree(elements.first().attr("data-cmp-data-layer"));
+        JsonNode expected = Utils.OBJECT_MAPPER.readTree(Utils.getResource("datalayer/sample-category-gallery.json"));
+        Assert.assertEquals(expected, result);
+
+        // Verify product items datalayer attributes
+        elements = doc.select(PRODUCTLIST_SELECTOR + PRODUCTCOLLECTION_GALLERY_ITEMS_SELECTOR);
+        result = Utils.OBJECT_MAPPER.readTree(elements.stream()
+            .map(e -> e.attr("data-cmp-data-layer"))
+            .collect(Collectors.joining(",", "[", "]")));
+        expected = Utils.OBJECT_MAPPER.readTree(Utils.getResource("datalayer/sample-category-items.json"));
+        Assert.assertEquals(expected, result);
     }
 
     @Test
-    public void testProductListPageWithPlaceholderData() throws ClientException {
+    public void testProductListPageWithPlaceholderData() throws ClientException, IOException {
         SlingHttpResponse response = adminAuthor.doGet(VENIA_CONTENT_US_EN_PRODUCTS_CATEGORY_PAGE + ".html", 200);
         Document doc = Jsoup.parse(response.getContent());
 
@@ -73,5 +94,19 @@ public class CategoryPageIT extends CommerceTestBase {
         // Verify breadcrumb: Home
         elements = doc.select(BREADCRUMB_ITEMS_SELECTOR);
         Assert.assertEquals(1, elements.size());
+
+        // Verify category gallery datalayer
+        elements = doc.select(PRODUCTLIST_GALLERY_SELECTOR);
+        JsonNode result = Utils.OBJECT_MAPPER.readTree(elements.first().attr("data-cmp-data-layer"));
+        JsonNode expected = Utils.OBJECT_MAPPER.readTree(Utils.getResource("datalayer/sample-category-gallery.json"));
+        Assert.assertEquals(expected, result);
+
+        // Verify product items datalayer attributes
+        elements = doc.select(PRODUCTLIST_SELECTOR + PRODUCTCOLLECTION_GALLERY_ITEMS_SELECTOR);
+        result = Utils.OBJECT_MAPPER.readTree(elements.stream()
+            .map(e -> e.attr("data-cmp-data-layer"))
+            .collect(Collectors.joining(",", "[", "]")));
+        expected = Utils.OBJECT_MAPPER.readTree(Utils.getResource("datalayer/placeholder-category-items.json"));
+        Assert.assertEquals(expected, result);
     }
 }
