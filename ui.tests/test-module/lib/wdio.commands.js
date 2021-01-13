1dfa8b6e44298462b4986173032504e509d3cd3b
Mark J. Becker <herzog31@users.noreply.github.com>
2021-01-13 16:03:24 +0100

    CIF-1674 - Add ui.tests for CIF component dialogs

--- ui.tests/test-module/lib/wdio.commands.js
+++ ui.tests/test-module/lib/wdio.commands.js
@@ -28,10 +28,9 @@ browser.addCommand('AEMLogin', function (username, password) {
     if ($('[class*="Accordion"] form').isExisting()) {
         try {
             $('#username').setValue(username);
-        }
-        // Form field not interactable, not visible
-        // Need to open the Accordion
-        catch (e) {
+        } catch (e) {
+            // Form field not interactable, not visible
+            // Need to open the Accordion
             $('[class*="Accordion"] button').click();
             browser.pause(500);
         }
@@ -55,33 +54,22 @@ browser.addCommand('AEMForceLogout', function () {
     $('form[name="login"]').waitForExist();
 });
 
-browser.addCommand('configureGraphqlClient', async function (factoryPid, properties) {
-    const auth = commons.getAuthenticatedRequestOptions(browser);
+/**
+ * Close all additional windows which might have been opened by a test.
+ */
+browser.addCommand('CloseOtherWindows', function () {
+    const handles = browser.getWindowHandles();
 
-    // Update OSGi config of GraphQL client
-    const configurations = await getOsgiConfigurations(auth, factoryPid);
-    const pid = configurations.length > 0 ? configurations[0].pid : '';
-    await editOsgiConfiguration(auth, pid, factoryPid, properties);
+    if (handles.length <= 1) return;
 
-    // Update OSGi config of Sling Authenticator to make GraphQL servlet reachable
-    await editOsgiConfiguration(auth, 'org.apache.sling.engine.impl.auth.SlingAuthenticator', null, {
-        'auth.sudo.cookie': 'sling.sudo',
-        'auth.sudo.parameter': 'sudo',
-        'auth.annonymous': 'false',
-        'sling.auth.requirements': [
-            '+/',
-            '-/libs/granite/core/content/login',
-            '-/etc.clientlibs',
-            '-/etc/clientlibs/granite',
-            '-/libs/dam/remoteassets/content/loginerror',
-            '-/apps/cif-components-examples/graphql'
-        ],
-        'sling.auth.anonymous.user': '',
-        'sling.auth.anonymous.password': 'unmodified',
-        'auth.http': 'preemptive',
-        'auth.http.realm': 'Sling+(Development)',
-        'auth.uri.suffix': '/j_security_check'
-    });
+    // Close all windows except for the first
+    for (let i = 1; i < handles.length; i++) {
+        browser.switchToWindow(handles[i]);
+        browser.closeWindow();
+    }
+
+    // Switch back to the first window
+    browser.switchToWindow(handles[0]);
 });
 
 // Returns file handle to use for file upload component,
@@ -110,14 +98,38 @@ browser.addCommand('AEMPathExists', function (baseUrl, path) {
         });
 });
 
-browser.addCommand('AEMDeleteAsset', function (assetPath) {
+browser.addCommand('AEMEditorLoaded', function () {
+    // Check for two conditions in the browse JavaScript context.
+    // Taken from https://git.corp.adobe.com/CQ/selenium-it-base/blob/45815ecf4c509aefcc9872ad1be4d978875ee81d/src/main/java/com/adobe/qe/selenium/pageobject/EditorPage.java#L191-L197
+    browser.waitUntil(() => {
+        // eslint-disable-next-line
+        return browser.execute(() => window && window.Granite && window.Granite.author && true);
+    });
+
+    browser.waitUntil(() => {
+        return browser.execute(() => {
+            // eslint-disable-next-line
+            if (window && window.Granite && window.Granite.author && true) {
+                // eslint-disable-next-line
+                var ns = window.Granite.author;
+                return ns.pageInfo && ns.pageInfo !== null;
+            }
+            return false;
+        });
+    });
+});
+
+/**
+ * Delete a page at the given path.
+ */
+browser.addCommand('AEMDeletePage', function (path) {
     let options = commons.getAuthenticatedRequestOptions(browser);
     Object.assign(options, {
         formData: {
             cmd: 'deletePage',
-            path: assetPath,
+            path,
             force: 'true',
-            '_charset_': 'utf-8'
+            _charset_: 'utf-8'
         }
     });
 
@@ -139,13 +151,37 @@ browser.addCommand('AEMSitesSetView', function (type) {
     browser.refresh();
 });
 
+/**
+ * Create a new page.
+ */
+browser.addCommand('AEMCreatePage', function (pageOptions) {
+    let options = commons.getAuthenticatedRequestOptions(browser);
+    const { title, name, parent, template } = pageOptions;
+    Object.assign(options, {
+        formData: {
+            './jcr:title': title,
+            pageName: name,
+            parentPath: parent,
+            template,
+            _charset_: 'utf-8'
+        }
+    });
+
+    return request.post(
+        url.resolve(config.aem.author.base_url, '/libs/wcm/core/content/sites/createpagewizard/_jcr_content'),
+        options
+    );
+});
+
 browser.addCommand('AEMSitesSetPageTitle', function (parentPath, name, title) {
     let originalTitle = '';
 
     // Navigate to page parent path
     browser.url(path.posix.join(AEM_SITES_PATH, parentPath));
     // Select sample page in the list
-    $(`[data-foundation-collection-item-id="${path.posix.join(parentPath, name)}"] [type="checkbox"]`).waitForClickable();
+    $(
+        `[data-foundation-collection-item-id="${path.posix.join(parentPath, name)}"] [type="checkbox"]`
+    ).waitForClickable();
     browser.pause(1000); // Avoid action bar not appearing after clicking checkbox
     $(`[data-foundation-collection-item-id="${path.posix.join(parentPath, name)}"] [type="checkbox"]`).click();
     // Access page properties form
@@ -162,31 +198,26 @@ browser.addCommand('AEMSitesSetPageTitle', function (parentPath, name, title) {
     return originalTitle;
 });
 
-async function getOsgiConfigurations(auth, factoryPid) {
-    const options = { ...auth, method: 'GET', uri: url.resolve(config.aem.author.base_url, '/system/console/configMgr/*.json'), json: true };
+browser.addCommand(
+    'waitAndClick',
+    function (args) {
+        this.waitForDisplayed();
+        this.click(args);
+    },
+    true
+);
 
-    const configurations = await request(options);
-    const configuration = configurations.filter(c => c.factoryPid === factoryPid);
-
-    return configuration;
-}
-
-async function editOsgiConfiguration(auth, pid, factoryPid, properties) {
-    const form = {
-        apply: 'true',
-        action: 'ajaxConfigManager',
-        propertylist: Object.keys(properties).join(','),
-        _charset_: 'utf-8',
-        ...properties
-    };
-    if (factoryPid) {
-        form.factoryPid = factoryPid;
+/**
+ * Opens the side panel in the AEM Sites editor if closed.
+ */
+browser.addCommand('EditorOpenSidePanel', function () {
+    const toggleButton = $('button[title="Toggle Side Panel"]');
+    toggleButton.waitForDisplayed();
+    const sidePanel = $('#SidePanel.sidepanel-opened');
+    if (!sidePanel.isDisplayed()) {
+        toggleButton.click();
     }
-
-    const options = { ...auth, method: 'POST', uri: url.resolve(config.aem.author.base_url, `/system/console/configMgr/${pid}`), form, simple: false, resolveWithFullResponse: true, useQuerystring: true };
-    const { statusCode } = await request(options);
-    expect(statusCode).toEqual(302);
-}
+});
 
 async function fileHandle(filePath) {
     if (config.upload_url) {
@@ -212,7 +243,7 @@ function fileHandleByUploadUrl(uploadUrl, filePath) {
                 options: {
                     filename: path.basename(filePath)
                 }
-            },
-        },
+            }
+        }
     });
 }
