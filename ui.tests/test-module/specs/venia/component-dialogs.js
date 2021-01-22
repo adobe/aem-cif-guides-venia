8c65d14a18b6e44529f9a3f8c334c89247162e6e
Mark J. Becker <herzog31@users.noreply.github.com>
2021-01-22 06:21:31 +0100

    CIF-1674 - Add UI tests for component dialogs (#96)
    * CIF-1674 - Add ui.tests for CIF component dialogs
    
    * CIF-1674 - Add ui.tests for CIF component dialogs
    
    * CIF-1674 - Add CIF snapshot dependency for Commerce Experience Fragement component
    
    * CIF-1674 - Add ui.tests for CIF component dialogs
    
    * CIF-1674 - Install connector for tests
    
    * CIF-1674 - Install connector for tests
    
    * CIF-1674 - Install connector for tests
    
    * CIF-1674 - Add retry
    
    * CIF-1674 - Add ui.tests for CIF component dialogs
    
    * CIF-1674 - Add ui.tests for CIF component dialogs
    
    * CIF-1674 - Add ui.tests for CIF component dialogs

---
+++ ui.tests/test-module/specs/venia/component-dialogs.js
@@ -0,0 +1,150 @@
+/*
+ *  Copyright 2020 Adobe Systems Incorporated
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+const config = require('../../lib/config');
+const { OnboardingDialogHandler, randomString } = require('../../lib/commons');
+
+describe('Component Dialogs', function() {
+    const editor_page = `${config.aem.author.base_url}/editor.html`;
+
+    let testing_page;
+    let onboardingHdler;
+
+    this.retries(2);
+
+    before(() => {
+        // Set window size to desktop
+        browser.setWindowSize(1280, 960);
+
+        // AEM Login
+        browser.AEMForceLogout();
+        browser.url(config.aem.author.base_url);
+        browser.AEMLogin(config.aem.author.username, config.aem.author.password);
+
+        // Enable helper to handle onboarding dialog popup
+        onboardingHdler = new OnboardingDialogHandler(browser);
+        onboardingHdler.enable();
+    });
+
+    after(function () {
+        // Disable helper to handle onboarding dialog popup
+        onboardingHdler.disable();
+    });
+
+    beforeEach(() => {
+        const pageName = `testing-${randomString()}`;
+        testing_page = `/content/venia/us/en/${pageName}`;
+        browser.AEMCreatePage({
+            title: 'Testing Page',
+            name: pageName,
+            parent: '/content/venia/us/en',
+            template: '/conf/venia/settings/wcm/templates/page-content'
+        });
+        browser.pause(1000);
+    });
+
+    afterEach(() => {
+        browser.AEMDeletePage(testing_page);
+    });
+
+    const addComponentToPage = (name, group = 'Venia - Commerce') => {
+        browser.url(`${editor_page}${testing_page}.html`);
+        browser.AEMEditorLoaded();
+        browser.EditorOpenSidePanel();
+
+        // Open the Components tab
+        $('coral-tab[title="Components"]').waitAndClick({ x: 1, y: 1 });
+
+        // Filter for Commerce components
+        $('#components-filter coral-select button').waitAndClick();
+        browser.pause(200);
+        $(`coral-selectlist-item[value="${group}"]`).waitAndClick();
+        expect($('#components-filter coral-select [handle=label]')).toHaveText(group);
+
+        // Drag category carousel component on page
+        const carouselCmp = $(`div[data-title="${name}"]`);
+        expect(carouselCmp).toBeDisplayed();
+        const dropTarget = $(`div[data-path="${testing_page}/jcr:content/root/container/container/*"]`);
+        carouselCmp.dragAndDrop(dropTarget, 1000);
+    };
+
+    const openComponentDialog = (node, trackingId) => {
+        // Open component dialog
+        const cmpPlaceholder = $(
+            `div[data-path="${testing_page}/jcr:content/root/container/container/${node}"]`
+        );
+        expect(cmpPlaceholder).toBeDisplayed();
+        cmpPlaceholder.click();
+        const configureButton = $('button[title="Configure"]');
+        expect(configureButton).toBeDisplayed();
+        configureButton.click();
+        const dialog = $(`coral-dialog[trackingfeature="${trackingId}"]`);
+        expect(dialog).toBeDisplayed();
+    };
+
+    it('opens the category carousel dialog', () => {
+        addComponentToPage('Category Carousel');
+        openComponentDialog('categorycarousel', 'aem:sites:components:dialogs:cif-core-components:categorycarousel:v1');
+
+        $('coral-multifield button').click();
+        expect($('coral-multifield-item').$('label=Category')).toBeDisplayed();
+    });
+
+    it('opens the commerce experience fragment dialog', () => {
+        addComponentToPage('Commerce Experience Fragment');
+        openComponentDialog('experiencefragment', 'aem:sites:components:dialogs:cif-core-components:experiencefragment:v1');
+
+        expect($('label=Experience fragment location name.')).toBeDisplayed();
+    });
+
+    it('opens the commerce teaser dialog', () => {
+        addComponentToPage('Commerce Teaser');
+        openComponentDialog('teaser', 'aem:sites:components:dialogs:cif-core-components:teaser:v1');
+
+        $('coral-tab-label=Link & Actions').click();
+        expect($('label=Link')).toBeDisplayed();
+    });
+
+    it('opens the featured categories dialog', () => {
+        addComponentToPage('Featured Categories');
+        openComponentDialog('featuredcategorylist', 'aem:sites:components:dialogs:cif-core-components:featuredcategorylist:v1');
+
+        $('coral-multifield button').click();
+        expect($('coral-multifield-item').$('label=Category')).toBeDisplayed();
+    });
+
+    it('opens the product carousel dialog', () => {
+        addComponentToPage('Product Carousel');
+        openComponentDialog('productcarousel', 'aem:sites:components:dialogs:cif-core-components:productcarousel:v1');
+
+        expect($('label=Carousel content')).toBeDisplayed();
+    });
+
+    it('opens the product teaser dialog', () => {
+        addComponentToPage('Product Teaser');
+        openComponentDialog('productteaser', 'aem:sites:components:dialogs:cif-core-components:productteaser:v1');
+
+        expect($('label=Select Product')).toBeDisplayed();
+    });
+
+    it('opens the releated products dialog', () => {
+        addComponentToPage('Related Products');
+        openComponentDialog('relatedproducts', 'aem:sites:components:dialogs:cif-core-components:relatedproducts:v1');
+
+        expect($('label=Base product - Leave empty to use the current product of the generic product page.')).toBeDisplayed();
+    });
+
+});
