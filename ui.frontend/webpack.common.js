c3f7a77363ff30e1f6d828c7f78f871c7f3622ec
Dirk Rudolph <drudolph@adobe.com>
2021-09-15 10:27:16 +0200

    fix build

--- ui.frontend/webpack.common.js
+++ ui.frontend/webpack.common.js
@@ -19,7 +19,6 @@ const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const pkg = require('./package.json');
 
 const SOURCE_ROOT = __dirname + '/src/main';
 const alias = Object.keys(pkg.dependencies)
