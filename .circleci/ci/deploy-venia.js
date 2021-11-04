8348f779b14ed7c71a4625d45d36de88fd6efb85
Dirk Rudolph <drudolph@adobe.com>
2021-11-04 16:17:58 +0100

    add mvn deploy to the release deploy stages

--- .circleci/ci/deploy-venia.js
+++ .circleci/ci/deploy-venia.js
@@ -29,16 +29,21 @@ ci.sh(
 ci.sh("mv tmp/**/ghr ./ghr");
 ci.sh("chmod +x ghr");
 
-ci.sh("mkdir -p artifacts"); // target folder for all the build artifacts
+// target folder for all the build artifacts
+ci.sh("mkdir -p artifacts");
 
-ci.stage(`Build and install Venia`);
-ci.sh(`mvn -B clean install -Pclassic`);
+ci.stage("Deploy Venia Sample Project to Maven Central");
+// deploy only the cloud artifacts
+ci.sh(`mvn -B clean deploy -Prelease,ossrh`)
 ci.sh(`cp all/target/${releaseArtifact}.all-${releaseVersion}.zip artifacts/${releaseArtifact}.all-${releaseVersion}.zip`);
-ci.sh(`cp classic/all/target/${releaseArtifact}.all-classic-${releaseVersion}.zip artifacts/${releaseArtifact}.all-${releaseVersion}-classic.zip`);
 
 ci.stage("Deploy Venia Sample Project to GitHub");
+// build also the classic artifacts for github
+ci.sh(`mvn -B clean install -Pclassic -pl classic/ui.config,classic/ui.content,classic/dispatcher,classic/all`);
+ci.sh(`cp classic/all/target/${releaseArtifact}.all-classic-${releaseVersion}.zip artifacts/${releaseArtifact}.all-${releaseVersion}-classic.zip`);
 ci.sh(`./ghr -t ${ci.env("GITHUB_TOKEN")} \
     -u ${ci.env("CIRCLE_PROJECT_USERNAME")} \
     -r ${ci.env("CIRCLE_PROJECT_REPONAME")} \
     -c ${ci.env("CIRCLE_SHA1")} \
     -replace ${ci.env("CIRCLE_TAG")} artifacts/`);
+
