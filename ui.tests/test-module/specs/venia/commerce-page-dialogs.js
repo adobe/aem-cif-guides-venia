15e87e880472edfabe43e3d9b96a4c899f07e71d
Levente Santha <levente@adobe.com>
2022-03-23 12:42:39 +0200

    CIF-2766 - Commerce property tab for category template page is broken
     * added UI tests for commerce page properties (landing, category, product pages)

---
+++ ui.tests/test-module/specs/venia/commerce-page-dialogs.js
@@ -0,0 +1,128 @@
+/*
+ *  Copyright 2022 Adobe Systems Incorporated
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+const config = require('../../lib/config');
+const { OnboardingDialogHandler } = require('../../lib/commons');
+
+describe('Commerce Page Component Dialog', function () {
+    const page_properties = `${config.aem.author.base_url}/mnt/overlay/wcm/core/content/sites/properties.html`;
+    const landing_page = '/content/venia/us/en';
+    const category_page = '/content/venia/us/en/products/category-page';
+    const product_page = '/content/venia/us/en/products/product-page';
+
+    let onboardingHdler;
+
+    this.retries(2);
+
+    before(() => {
+        // Set window size to desktop
+        browser.setWindowSize(1280, 960);
+
+        // AEM Login
+        browser.AEMForceLogout();
+        browser.url(config.aem.author.base_url);
+        browser.AEMLogin(config.aem.author.username, config.aem.author.password);
+
+        // Enable helper to handle onboarding dialog popup
+        onboardingHdler = new OnboardingDialogHandler(browser);
+        onboardingHdler.enable();
+    });
+
+    after(function () {
+        // Disable helper to handle onboarding dialog popup
+        onboardingHdler.disable();
+    });
+
+    it('Landing page dialog extensions are displayed', () => {
+        // Landing page properties
+        browser.url(`${page_properties}?item=${encodeURIComponent(`${landing_page}`)}`);
+
+        // Open Commerce tab
+        let commerceTab = $('coral-tab-label=Commerce');
+        expect(commerceTab).toBeClickable();
+        commerceTab.click();
+
+        let commercePanel = $('coral-panelstack coral-panel.is-selected[role="tabpanel"]');
+        expect(commercePanel).toBeDisplayed();
+
+        expect(commercePanel.$('.coral-Form-fieldset-legend=Commerce Pages')).toBeDisplayed();
+
+        let fields = commercePanel.$$('.coral-Form-fieldwrapper');
+
+        // check fields
+        expect(fields.length).toEqual(5);
+        expect(fields[0].$('label')).toHaveText('Product Page');
+        expect(fields[0].$('foundation-autocomplete[name="./cq:cifProductPage"]')).toExist();
+        expect(fields[1].$('label')).toHaveText('Category Page');
+        expect(fields[1].$('foundation-autocomplete[name="./cq:cifCategoryPage"]')).toExist();
+        expect(fields[2].$('label')).toHaveText('Search Results Page');
+        expect(fields[2].$('foundation-autocomplete[name="./cq:cifSearchResultsPage"]')).toExist();
+        expect(fields[3].$('label')).toHaveText('Address Book Page');
+        expect(fields[3].$('foundation-autocomplete[name="./cq:cifAddressBookPage"]')).toExist();
+        expect(fields[4].$('label')).toHaveText('My Account Page');
+        expect(fields[4].$('foundation-autocomplete[name="./cq:cifMyAccountPage"]')).toExist();
+    });
+
+    it('Category page dialog extensions are displayed', () => {
+        // Category page properties
+        browser.url(`${page_properties}?item=${encodeURIComponent(`${category_page}`)}`);
+
+        // Open Commerce tab
+        let commerceTab = $('coral-tab-label=Commerce');
+        expect(commerceTab).toBeClickable();
+        commerceTab.click();
+
+        let commercePanel = $('coral-panelstack coral-panel.is-selected[role="tabpanel"]');
+        expect(commercePanel).toBeDisplayed();
+
+        expect(commercePanel.$('.coral-Form-fieldset-legend=Commerce Settings')).toBeDisplayed();
+
+        let fields = commercePanel.$$('.coral-Form-fieldwrapper');
+
+        // check fields
+        expect(fields.length).toEqual(2);
+        expect(fields[0].$('category-field')).toExist();
+        expect(fields[0].$('coral-taglist[name="./selectorFilter"]')).toExist();
+        expect(fields[1].$('coral-checkbox')).toExist();
+        expect(fields[1].$('input[name="./includesSubCategories"]')).toExist();
+    });
+
+    it('Product page dialog extensions are displayed', () => {
+        // Product page properties
+        browser.url(`${page_properties}?item=${encodeURIComponent(`${product_page}`)}`);
+
+        // Open Commerce tab
+        let commerceTab = $('coral-tab-label=Commerce');
+        expect(commerceTab).toBeClickable();
+        commerceTab.click();
+
+        let commercePanel = $('coral-panelstack coral-panel.is-selected[role="tabpanel"]');
+        expect(commercePanel).toBeDisplayed();
+
+        expect(commercePanel.$('.coral-Form-fieldset-legend=Commerce Settings')).toBeDisplayed();
+
+        let fields = commercePanel.$$('.coral-Form-fieldwrapper');
+
+        // check fields
+        expect(fields.length).toEqual(3);
+        expect(fields[0].$('product-field')).toExist();
+        expect(fields[0].$('coral-taglist[name="./selectorFilter"]')).toExist();
+        expect(fields[1].$('category-field')).toExist();
+        expect(fields[1].$('coral-taglist[name="./useForCategories"]')).toExist();
+        expect(fields[2].$('coral-checkbox')).toExist();
+        expect(fields[2].$('input[name="./includesSubCategories"]')).toExist();
+    });
+});
