4870da7b97efb4a829ba77e6a8be647f26e8c7f7
levente <levente@qadobe.com>
2023-11-08 14:04:36 +0200

    SITES-16602 - Remove sort by position option in the Search Results Component
     * fixing test failures

--- ui.tests/test-module/specs/venia/productcollection.js
+++ ui.tests/test-module/specs/venia/productcollection.js
@@ -43,10 +43,10 @@ describe('Product Collection Component', function () {
         // perform a search
         browser.url(`${config.aem.author.base_url}/content/venia/us/en/search.html?search_query=dress`);
 
-        const categoryFilterList = 'label[for="category_id"] ~ .productcollection__filter-items';
+        const categoryFilterList = 'label[for="category_uid"] ~ .productcollection__filter-items';
 
         // check category filter is closed
-        const categoryFilter = $('label[for="category_id"]');
+        const categoryFilter = $('label[for="category_uid"]');
         expect(categoryFilter).toBeDisplayed();
         expect($(categoryFilterList)).not.toBeDisplayed();
 
@@ -64,9 +64,9 @@ describe('Product Collection Component', function () {
         browser.url(
             `${config.aem.author.base_url}/content/venia/us/en/products/category-page.html/venia-accessories.html`
         );
-        expect($('label[for="category_id"]')).toBeDisplayed();
+        expect($('label[for="category_uid"]')).toBeDisplayed();
         const categoryFilterItems = $$(
-            'label[for="category_id"] ~ .productcollection__filter-items .productcollection__filter-item'
+            'label[for="category_uid"] ~ .productcollection__filter-items .productcollection__filter-item'
         );
         expect(categoryFilterItems.length).toBe(3);
 
@@ -74,6 +74,6 @@ describe('Product Collection Component', function () {
         browser.url(
             `${config.aem.author.base_url}/content/venia/us/en/products/category-page.html/venia-accessories/venia-jewelry.html`
         );
-        expect($('label[for="category_id"]')).not.toBeDisplayed();
+        expect($('label[for="category_uid"]')).not.toBeDisplayed();
     });
 });
