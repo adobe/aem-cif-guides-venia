62d93963c7071436bdb9a54794044b1354b66d2d
John Jung <johnj@adobe.com>
2023-11-13 14:47:26 -0500

    add Authorization bearer to the headers

--- ui.apps/src/main/content/jcr_root/apps/venia/components/commerce/searchbar/clientlibs/js/searchbar.js
+++ ui.apps/src/main/content/jcr_root/apps/venia/components/commerce/searchbar/clientlibs/js/searchbar.js
@@ -66,13 +66,45 @@ const dataServicesStoreConfigurationContextQuery = `
      }
    `;
 
+const getCookie = (cookieName) => {
+  const cookie = document.cookie.match(
+    `(^|[^;]+)\\s*${cookieName}\\s*=\\s*([^;]+)`
+  );
+  return cookie ? cookie.pop() : "";
+};
+
+const getLoginToken = () => {
+  const key = "M2_VENIA_BROWSER_PERSISTENCE__signin_token";
+  let token = getCookie("cif.userToken") || "";
+
+  try {
+    const lsToken = JSON.parse(localStorage.getItem(key));
+    if (lsToken && lsToken.value) {
+      const timestamp = new Date().getTime();
+      if (timestamp - lsToken.timeStored < lsToken.ttl * 1000) {
+        token = lsToken.value.replace(/"/g, "");
+      }
+    }
+  } catch (e) {
+    console.error(`Login token at ${key} is not valid JSON.`);
+  }
+  return token;
+};
+
 async function getGraphQLQuery(query, variables = {}) {
   const graphqlEndpoint = `/api/graphql`;
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  const loginToken = getLoginToken();
+  if (loginToken) {
+    headers["Authorization"] = `Bearer ${loginToken}`;
+  }
+
   const response = await fetch(graphqlEndpoint, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers,
     body: JSON.stringify({
       query,
       variables,
@@ -183,7 +215,6 @@ class SearchBar {
       return;
     }
 
-
     // initialize live-search
     new window.LiveSearchAutocomplete({
       environmentId: dataServicesStorefrontInstanceContext.environment_id,
@@ -195,8 +226,10 @@ class SearchBar {
         minQueryLength: "2",
         currencySymbol: dataServicesStoreConfigurationContext.currency_symbol,
         currencyRate: dataServicesStoreConfigurationContext.currency_rate,
-        displayOutOfStock: dataServicesStoreConfigurationContext.display_out_of_stock,
-        allowAllProducts: dataServicesStoreConfigurationContext.allow_all_products,
+        displayOutOfStock:
+          dataServicesStoreConfigurationContext.display_out_of_stock,
+        allowAllProducts:
+          dataServicesStoreConfigurationContext.allow_all_products,
       },
       context: {
         customerGroup: dataServicesStorefrontInstanceContext.customer_group,
