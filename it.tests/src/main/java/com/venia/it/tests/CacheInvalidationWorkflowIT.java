e25a4b5d8052205b64447c21d962ec225de01f84
Satish-Pothabathula <del60718@adobe.com>
2025-09-05 15:32:05 +0530

    IT test for cache clear

--- it.tests/src/main/java/com/venia/it/tests/CacheInvalidationWorkflowIT.java
+++ it.tests/src/main/java/com/venia/it/tests/CacheInvalidationWorkflowIT.java
@@ -13,7 +13,7 @@
  ~ See the License for the specific language governing permissions and
  ~ limitations under the License.
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-package com.adobe.cq.commerce.it.http;
+package com.venia.it.tests;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
