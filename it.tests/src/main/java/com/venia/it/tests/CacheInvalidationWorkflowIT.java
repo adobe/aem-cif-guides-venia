cea11ae8a5390bf2391988fd4508036ef53ae556
Satish-Pothabathula <del60718@adobe.com>
2025-09-09 23:26:09 +0530

    check

--- it.tests/src/main/java/com/venia/it/tests/CacheInvalidationWorkflowIT.java
+++ it.tests/src/main/java/com/venia/it/tests/CacheInvalidationWorkflowIT.java
@@ -10,7 +10,7 @@
  ~ Unless required by applicable law or agreed to in writing, software
  ~ distributed under the License is distributed on an "AS IS" BASIS,
  ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- ~ See the License for the specific language governing permissions and
+ ~ See the License for the specific language governing permissions
  ~ limitations under the License.
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 package com.venia.it.tests;
@@ -345,7 +345,7 @@ public class CacheInvalidationWorkflowIT extends CommerceTestBase {
 
             String payload = String.format(
                     "{\n" +
-                            "    \"customProductSkus\": [\"%s\"],\n" +
+                            "    \"productSkus\": [\"%s\"],\n" +
                             "    \"storePath\": \"%s\"\n" +
                             "}", TEST_PRODUCT_SKU, STORE_PATH);
 
