3fbb9be604c23cee09b1e69aa589e72d3d93fbe7
Alwin Joseph <aljoseph@adobe.com>
2025-10-02 08:13:14 +0530

    SITES-34368: Code review changes

--- core/src/main/java/com/venia/core/models/commerce/MyStartupService.java
+++ core/src/main/java/com/venia/core/models/commerce/MyStartupService.java
@@ -4,6 +4,8 @@ import com.shopify.graphql.support.AbstractResponse;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A simple OSGi component that runs code on bundle startup and shutdown.
@@ -11,17 +13,19 @@ import org.osgi.service.component.annotations.Deactivate;
 @Component(immediate = true)
 public class MyStartupService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MyStartupService.class);
+
     @Activate
     protected void activate() {
         // Set system property to enable custom simple fields when bundle starts
         System.setProperty(AbstractResponse.UNLOCK_CUSTOM_UNSAFE_FIELDS_PROPERTY, "true");
-        System.out.println("Bundle started!");
+        LOG.info("MyStartupService bundle started");
     }
 
     @Deactivate
     protected void deactivate() {
         // Set system property to disable custom simple fields when bundle stops
         System.setProperty(AbstractResponse.UNLOCK_CUSTOM_UNSAFE_FIELDS_PROPERTY, "false");
-        System.out.println("Bundle stopped!");
+        LOG.info("MyStartupService bundle stopped");
     }
 }
