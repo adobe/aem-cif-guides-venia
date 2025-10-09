ed5ec926e125c50181ec6b3c2f6de74be0e1e386
Satish-Pothabathula <del60718@adobe.com>
2025-10-09 21:47:11 +0530

    Revert "removing the exclud efun"
    This reverts commit feeaaf7b206ed5567364075e50ff296316dfd88d.

--- it.tests/src/main/java/com/venia/it/tests/ProductPageIT.java
+++ it.tests/src/main/java/com/venia/it/tests/ProductPageIT.java
@@ -113,6 +113,7 @@ public class ProductPageIT extends CommerceTestBase {
     }
 
     @Test
+    @Category(IgnoreOnCloud.class)
     public void testProductPageWithSampleDataForGroupedProduct65() throws ClientException, IOException {
         System.out.println("=== ENVIRONMENT DEBUG: Running testProductPageWithSampleDataForGroupedProduct65 ===");
         System.out.println("=== ENVIRONMENT DEBUG: This test has @Category(IgnoreOnCloud.class) ===");
