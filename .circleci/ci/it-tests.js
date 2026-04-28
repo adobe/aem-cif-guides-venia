a77f2f57be28454bc354206d3a603b0121a07acc
spothabathula <del60718@adobe.com>
2026-04-28 18:23:41 +0530

    image update

--- .circleci/ci/it-tests.js
+++ .circleci/ci/it-tests.js
@@ -127,7 +127,7 @@ try {
             extras += ` --bundle com.adobe.commerce.cif:core-cif-components-examples-bundle:${cifVersion}:jar`;
         }
 
-        const maxMetaspace = classifier == 'lts' ? '-XX:MaxMetaspaceSize=256m' : '-XX:MaxPermSize=256m';
+        const maxMetaspace =  '-XX:MaxMetaspaceSize=256m';
 
         // Start CQ
         ci.sh(`./qp.sh -v start --id author --runmode author --port 4502 --qs-jar /home/circleci/cq/author/cq-quickstart.jar \
