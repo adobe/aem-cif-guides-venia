e6295ced9dcd1c73745a5d64aff555c0ba7d8cd5
spothabathula <del60718@adobe.com>
2026-05-21 20:00:06 +0530

    Revert "removing the LTS check"
    This reverts commit b42f43e2cf8178fd0fe76d87269f175c27bef70c.

--- ui.tests/test-module/specs/venia/component-dialogs.js
+++ ui.tests/test-module/specs/venia/component-dialogs.js
@@ -153,8 +153,11 @@ describe('Component Dialogs', function () {
         expect($('label=Select Product')).toBeDisplayed();
 
         let fields = $$('.cq-dialog-content .coral-Form-fieldwrapper');
-
-        expect(fields.length).toEqual(8);
+        if (config.aem.type === 'lts') {
+            expect(fields.length).toEqual(7);
+        } else {
+            expect(fields.length).toEqual(8);
+        }
 
         // fields of the OOTB component
         expect(fields[0].$('product-field')).toBeDisplayed();
